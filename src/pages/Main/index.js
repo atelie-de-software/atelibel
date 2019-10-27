6eb5dce009097e99976830049232742280813c91
davimattos <davimattosdev@gmail.com>
2019-10-27 13:36:40 -0300

    configuracao minima do decibel

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import RNSoundLevel from 'react-native-sound-level';
+import {View, Text, Vibration} from 'react-native';
 
-import {View} from 'react-native';
+// import { Container } from './styles';
 
-export default function Main() {
-  return <View />;
+export default function Atelibel() {
+  const [decibelPeace] = useState(-30);
+  const [decibel, setDecibel] = useState('');
+  useEffect(() => {
+    RNSoundLevel.start();
+    RNSoundLevel.onNewFrame = data => {
+      setDecibel(data.value);
+    };
+    return () => {
+      RNSoundLevel.stop();
+    };
+  });
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: decibel > decibelPeace ? 'red' : 'yellow',
+      }}>
+      {decibel > decibelPeace && (
+        <Text style={{fontSize: 32, color: 'white', fontWeight: 'bold'}}>
+          Danger!
+        </Text>
+      )}
+    </View>
+  );
 }
